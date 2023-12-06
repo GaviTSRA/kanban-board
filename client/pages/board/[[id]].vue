139444a6943643ba0e373905d4a50404004615fe
gavi <gavitsra@gmail.com>
2023-12-06 08:05:09 +0100

    add back button

--- client/pages/board/[[id]].vue
+++ client/pages/board/[[id]].vue
@@ -33,6 +33,8 @@
 <template>
     <div>
         <div class="header">
+            <!-- TODO icon? -->
+            <NuxtLink to="/" class="back">Back</NuxtLink>
             <p class="title">{{ board.title }}</p>
             <p class="description">{{ board.description }}</p>
         </div>
@@ -40,11 +42,18 @@
 </template>
 
 <style scoped>
+    .back {
+        margin: auto 10px;
+        margin-right: 2rem;
+        border-radius: 3px;
+        padding: 2px 5px;
+    }
     .header {
         display: flex;
         flex-direction: row;
         position: absolute;
         top: 0;
+        margin: 0 auto;
         width: 100vw;
         height: 7vh;
         background-color: var(--color-background-mute);

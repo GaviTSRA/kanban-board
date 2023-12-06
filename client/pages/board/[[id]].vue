4bcee2553a17879aaa0eeef8613b5e9784683c70
gavi <gavitsra@gmail.com>
2023-12-06 08:22:34 +0100

    move board title bar to own component

--- client/pages/board/[[id]].vue
+++ client/pages/board/[[id]].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import BoardTitleBar from '~/components/BoardTitleBar.vue';
+
     const route = useRoute()
 
     let board = ref({
@@ -32,42 +34,10 @@
 
 <template>
     <div>
-        <div class="header">
-            <!-- TODO icon? -->
-            <NuxtLink to="/" class="back">Back</NuxtLink>
-            <p class="title">{{ board.title }}</p>
-            <p class="description">{{ board.description }}</p>
-        </div>
+        <BoardTitleBar :board="board"/>
     </div>
 </template>
 
 <style scoped>
-    .back {
-        margin: auto 10px;
-        margin-right: 2rem;
-        border-radius: 3px;
-        padding: 2px 5px;
-    }
-    .header {
-        display: flex;
-        flex-direction: row;
-        position: absolute;
-        top: 0;
-        margin: 0 auto;
-        width: 100vw;
-        height: 7vh;
-        background-color: var(--color-background-mute);
-    }
 
-    .title {
-        font-size: 1.5rem;
-        margin: 10px;
-        color: aqua
-    }
-
-    .description {
-        margin: auto 0;
-        margin-left: 1rem;
-        width: 30vw;
-    }
 </style>

8d4df037959a21cd15321ff425ee392d9542969a
gavi <gavitsra@gmail.com>
2023-12-15 13:03:12 +0100

    maybe fix checklist bug

--- client/components/EditableText.vue
+++ client/components/EditableText.vue
@@ -47,6 +47,8 @@
 
     function looseFocus() {
         setTimeout(() => {
+            if (editing.value)
+                done(props.textarea)
             isFocused.value = false
         }, 100)
     }
@@ -70,8 +72,8 @@
 <template>
     <form @submit.prevent="()=>done(0)" :class="{container: true, center: props.center}">
         <p @click="detectDoubleClick" v-if="!editing">{{ props.text }}</p>
-        <input :maxlength="maxlength" @focus="isFocused = true" @blur="looseFocus" ref="input" class="editable" v-model="text" v-show="editing && !props.textarea" v-click-away="()=>done(false)"/>
-        <textarea :maxlength="maxlength" ref="textarea" @focus="isFocused = true" @blur="looseFocus" class="editable" v-model="text" v-show="editing && props.textarea" v-click-away="()=>done(true)"/>
+        <input :maxlength="maxlength" @focus="isFocused = true" @blur="looseFocus" ref="input" class="editable" v-model="text" v-show="editing && !props.textarea" v-all-click-away="()=>done(false)"/>
+        <textarea :maxlength="maxlength" ref="textarea" @focus="isFocused = true" @blur="looseFocus" class="editable" v-model="text" v-show="editing && props.textarea" v-all-click-away="()=>done(true)"/>
     </form>
 </template>
 
